6c281b6b6394b1d70cb09b262d6da6bea6e1f44b
wq_pc <wuq_xd@163.com>
2020-03-25 01:13:36 +0800

    【配置文件】【路由配置】空路径时导航到 home 模块

--- vue-admin-template/src/router/index.js
+++ vue-admin-template/src/router/index.js
@@ -46,7 +46,7 @@ export const constantRoutes = [
   {
     path: '/',
     component: Layout,
-    redirect: '/dashboard',
+    redirect: '/home',
     children: [{
       path: 'dashboard',
       name: 'Dashboard',
