a7ce4741950f79d1259bf229da7881ad13c24e12
wq_pc <wuq_xd@163.com>
2020-03-25 01:41:24 +0800

    【业务模块】【首页】引入 echarts 图表

--- vue-admin-template/src/views/home/index.vue
+++ vue-admin-template/src/views/home/index.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home-container">
-      home
+      <div id="chart" :style="{height: '200px',width: '300px'}" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import echarts from 'echarts';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      chart: null,
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  mounted() {
+    this.initChart()
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(document.getElementById('chart'));
+      this.chart.setOption({
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+      });
+    }
   }
 }
 </script>
