d512d240c0e728ce5744c78ab87c20a7cb1e4c00
wq_pc <wuq_xd@163.com>
2020-03-25 01:12:39 +0800

    【公共组件】主导航文件 - 由 el-menu 组件实现

--- vue-admin-template/src/layout/components/MainMenu.vue
+++ vue-admin-template/src/layout/components/MainMenu.vue
@@ -1,43 +1,33 @@
 <template>
   <el-menu
-    :default-active="activeIndex2"
+    :default-active="activeIndex"
     class="el-menu-demo"
     mode="horizontal"
     @select="handleSelect"
     background-color="#545c64"
     text-color="#fff"
     active-text-color="#ffd04b"
-  >
-    <!-- <el-menu-item index="1">处理中心</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">我的工作台</template>
-      <el-menu-item index="2-1">选项1</el-menu-item>
-      <el-menu-item index="2-2">选项2</el-menu-item>
-      <el-menu-item index="2-3">选项3</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="2-4-1">选项1</el-menu-item>
-        <el-menu-item index="2-4-2">选项2</el-menu-item>
-        <el-menu-item index="2-4-3">选项3</el-menu-item>
+  > 
+    <template v-for="(item) in menuConfig" :index="item.name">
+      <el-submenu v-if="!item.hidden && item.children" :key="item.name" :index="item.name" :disabled="item.disabled">
+        <template slot="title">{{item.title}}</template>
+        <template v-for="(subItem) in item.children">
+          <el-menu-item v-if="!subItem.hidden" :key="subItem.name" :index="subItem.name" :disabled="subItem.disabled">{{subItem.title}}</el-menu-item>
+        </template>
       </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-    <el-menu-item index="4">
-      <a href="https://www.ele.me" target="_blank">订单管理</a>
-    </el-menu-item> -->
-    <el-menu-item v-for="(item) in menuConfig" :key="item.name" :index="item.name">
-      <a :href="`#${item.route}`" target="_blank">{{item.title}}</a>
-    </el-menu-item>
+      <el-menu-item v-if="!item.hidden && !item.children" :key="item.name" :index="item.name" :disabled="item.disabled">
+        {{item.title}}
+      </el-menu-item>
+    </template>
   </el-menu>
 </template>
 <script>
-import { menuConfig } from '../../config/menu';
+import { menuConfig } from '@/config/menu';
 export default {
   name: 'MainMenu',
   data() {
     return {
-      activeIndex: "1",
-      activeIndex2: "1",
+      activeIndex: "home",
       menuConfig: menuConfig,
     };
   },
