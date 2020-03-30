b3c3dd597e195e28b7fe4faedc148c544c946452
fbm <wuq_xd@163.com>
2020-03-30 21:19:22 +0800

    【业务模块】getVolumeList 接口测试

---
+++ vue-admin-template/src/views/provision/volume/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app-container">
+    
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+import { getVolumeList } from '@/api/provision/volume.js'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  created() {
+    this.fetchData();
+    this.volumeList();
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    },
+    volumeList() {
+      getVolumeList().then();
+    }
+  }
+}
+</script>
