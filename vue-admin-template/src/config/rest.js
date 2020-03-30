088e11a9384edbf8cf2f0a2b9676da347948da26
fbm <wuq_xd@163.com>
2020-03-30 21:16:13 +0800

    【接口配置文件】rest文件中统一管理各模块接口

--- vue-admin-template/src/config/rest.js
+++ vue-admin-template/src/config/rest.js
@@ -1,21 +1,24 @@
-const API_V1 = {
+const rest = {
   home: {
-    get: '/home'
+    get: '/home',
+    post: '/home'
   },
   provision: {
     volume: {
-      get: '/provision/volumes'
+      get: { url: '/provision/volumes', name: '卷列表' },
+      post: '/provision/volumes'
+    },
+    host: {
+      post: '/provision/volumes'
     }
   }
 }
-
-function createApi_V1(api) {
-  console.log(Object.entries(api))
-}
-
+// 版本控制
+function createApi_V1(api) {}
 function ApiFactory() {
   createApi_V1(API_V1);
   return {};
 }
-const rest = new ApiFactory();
+// const rest = new ApiFactory();
+
 export default rest
