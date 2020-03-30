0a91f3ac33f9fbb32378a659890df59322779b3f
fbm <wuq_xd@163.com>
2020-03-30 21:14:43 +0800

    【api】新增卷列表接口

--- vue-admin-template/src/api/provision/volume.js
+++ vue-admin-template/src/api/provision/volume.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 import restApi from '@/config/rest'
 
-export function volume_list() {
+export function getVolumeList() {
   return request({
     url: restApi.provision.volume,
     method: 'get'
